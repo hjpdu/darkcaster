5de7be1a0cfd942a84b4ff8dd1f603e3ad6e7dd7
Harryson Pointdujour <Harryson Pointdujour>
2016-10-19 18:44:04 -0400

    refactored to index router from index.js

--- index.js
+++ index.js
@@ -4,21 +4,18 @@ var server = express();
 var logger = require('./middleware/logger.js');
 var authorize = require('./middleware/auth.js');
 
-
 var forecastRouter = require('./routers/forecast.router.js');
+var indexRouter = require('./routers/index.router.js');
 
 var port = process.env.PORT || 8080;
 
 server.use(express.static(__dirname + '/public'));
 server.use(logger);
 server.use(cors());
-server.use(authorize);
+// server.use(authorize);
 
-
-server.get('/', function(request, response){
-  response.sendFile('public/html/index.html', {root:__dirname});
-});
 server.use(forecastRouter);
+server.use(indexRouter);
 
 server.listen(port, function(){
   console.log('Now listening on port...', port);
