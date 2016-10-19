4d8131db9cb4702bf468d0b4e22d710ac69298e9
Harryson Pointdujour <Harryson Pointdujour>
2016-10-19 19:01:27 -0400

    fixed error with forecast versus index

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 function authorize(request,response,next){
   var secretPassphrase = request.headers.secret;
-  var mySecretPassphrase = process.env.PASS || require('../config.js').mySecretPassphrase;
+  var mySecretPassphrase = process.env.PASS || require('../config.js').secretPassphrase;
 
   if(!secretPassphrase || secretPassphrase !== mySecretPassphrase){
     response.status(403).json({
