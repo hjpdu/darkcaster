1a200dd3d5160dbe2dd4d7f188d3bb1b991e60b8
Harryson Pointdujour <Harryson Pointdujour>
2016-10-18 20:04:16 -0400

    add authorization route

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 var config = {
-  apiKey:"4e59e08f785fbbecd4fc80cc01fd0a33"
+  apiKey:"4e59e08f785fbbecd4fc80cc01fd0a33",
+  secretPassphrase: 'its a secret'
 };
 
 module.exports = config;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var cors = require('cors');
 var server = express();
 var logger = require('./middleware/logger.js');
+var authorize = require('./middleware/auth.js');
 
 
 var forecastRouter = require('./routers/forecast.router.js');
@@ -11,6 +12,7 @@ var port = process.env.PORT || 8080;
 server.use(express.static(__dirname + '/public'));
 server.use(logger);
 server.use(cors());
+server.use(authorize);
 
 
 server.get('/', function(request, response){
