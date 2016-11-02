69ad0757cf48048a7797b7287b324a1c975a475a
Harryson Pointdujour <Harryson Pointdujour>
2016-11-01 20:22:31 -0400

    hooked up minutely controller backend

--- public/js/controllers/minutely.controller.js
+++ public/js/controllers/minutely.controller.js
@@ -1,4 +1,22 @@
 (function() {
   angular.module('forecast-app')
   .controller('MinutelyController', MinutelyController);
+
+  MinutelyController.$inject = ['$scope', 'WeatherService'];
+
+  function MinutelyController($scope,WeatherService){
+    $scope.getWeather = getWeather;
+    $scope.weather = WeatherService.weatherData;
+
+    $scope.$watch(function(){
+      return WeatherService.weatherData;
+    }, function(newVal, oldVal){
+      $scope.weather = WeatherService.weatherData;
+    });
+
+
+    function getWeather(latitude,longitude){
+      WeatherService.getWeather(latitude,longitude);
+    }
+  }
 }());
